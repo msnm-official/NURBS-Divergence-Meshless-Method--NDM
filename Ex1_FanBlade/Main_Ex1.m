604b412eb70907245613bdb1e790dcbf40359f2d
Malaysian Society for Numerical Methods (MSNM) <official.msnm@gmail.com>
2024-07-04 09:14:30 +0800

    Update Main_Ex1.m

--- Ex1_FanBlade/Main_Ex1.m
+++ Ex1_FanBlade/Main_Ex1.m
@@ -82,8 +82,6 @@ Plot('Node',node_xy,'k.',25)
 
 % Get shape function to calculate dNx, dNf
 h = max([1/mx 1/my]);
-ac = 1;
-dc = h;
 rc = 4*h;
 [N,dNdx,dNdy] = MLS_SF(P,node_ab,rc);
 
@@ -142,7 +140,6 @@ end
 % Impose boundary conditions (Neumann)
 % ----------------------
 
-Line = 0;
 for i = 1:size(S,1) %loop for sub-area
     s = S(i,:);     %related sides for loop i
     disp([i s])
